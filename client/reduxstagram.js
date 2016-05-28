341558c38093a856240771e2203e3d61af5e12c8
Lucas <lucas@agtlucas.com>
2016-05-28 19:16:03 -0300

    Updates

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 
-import css from './styles/style.styl'
+import './styles/style.styl'
+
+render(<p>Yo!</p>, document.getElementById('root'))
