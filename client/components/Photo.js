295ded1f568de7e4bb4edfa0b54e29e36dd6a4a8
Lucas <lucas@agtlucas.com>
2016-06-02 19:49:44 +0200

    Update PhotoGrid

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
+import CSSTransitionGroup from 'react-addons-css-transition-group'
 
 class Photo extends Component {
   render () {
@@ -10,6 +11,10 @@ class Photo extends Component {
           <Link to={`/view/${post.code}`}>
           <img src={post.display_src} alt={post.caption} className='grid-photo' />
           </Link>
+
+          <CSSTransitionGroup transitionName='like' transitionEnterTimeout={500} transitionLeaveTimeout={500}>
+            <span key={post.likes} className='likes-heart'>{post.likes}</span>
+          </CSSTransitionGroup>
         </div>
       </figure>
     )
