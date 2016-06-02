6854362f38282e49065392dcd121e26cb7bb742a
Lucas <lucas@agtlucas.com>
2016-06-02 19:43:42 +0200

    Update PhotoGrid

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -6,7 +6,7 @@ class PhotoGrid extends Component {
     return (
       <div className='photo-grid'>
         {this.props.posts.map((post, i) => {
-          return <Photo {...this.props} />
+          return <Photo {...this.props} key={i} i={i} post={post} />
         })}
       </div>
     )
