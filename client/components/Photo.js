a00464977271ec25d2fbab99c821c83d1d989e98
Lucas <lucas@agtlucas.com>
2016-06-02 19:58:00 +0200

    Update PhotoGrid

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -16,6 +16,19 @@ class Photo extends Component {
             <span key={post.likes} className='likes-heart'>{post.likes}</span>
           </CSSTransitionGroup>
         </div>
+
+        <figcaption>
+          <p>{post.caption}</p>
+          <div className='control-buttons'>
+            <button className='likes'>&hearts; {post.likes}</button>
+          <Link className='button' to={`/view/${post.code}`}>
+              <span className='comment-count'>
+                <span className='speech-bubble'></span>
+                {comments[post.code] ? comments[post.code].length : 0}
+              </span>
+            </Link>
+          </div>
+        </figcaption>
       </figure>
     )
   }
