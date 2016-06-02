957ec8ce290c727ac8f88df4eebc2a5c8e5dbbbf
Lucas <lucas@agtlucas.com>
2016-06-02 19:45:22 +0200

    Update PhotoGrid

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router'
 
 class Photo extends Component {
   render () {
+    const { post, i, comments } = this.props
     return (
       <figure className='grid-figure'>
-        I'm a photo!
+        <div className='grid-photo-wrap'>
+          <Link to={`/view/${post.code}`}>
+          {post.caption}
+          </Link>
+        </div>
       </figure>
     )
   }
